a178d4ddeb29b6dc0e894a26ab92ecffc5ee1e5d
Mike Vegeto <michael.vegeto@gmail.com>
2017-07-07 00:03:08 -0400

    test credentials setup

--- test/main.js
+++ test/main.js
@@ -1,3 +1,10 @@
+const AWS = require('aws-sdk')
+
+AWS.config = new AWS.Config()
+AWS.config.accessKeyId = 'accessKey'
+AWS.config.secretAccessKey = 'secretKey'
+AWS.config.region = 'us-east-1'
+
 if (process.argv[2] === '--debug' || process.argv[2] === 'debug') {
   process.env.DEBUG = process.argv[3] || 'simdown*'
 }
