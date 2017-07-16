e0b0e6af8da30739435e95d375f3ff36563659eb
Mike Vegeto <michael.vegeto@gmail.com>
2017-07-15 21:47:38 -0400

    get cli basics working

--- bin/main.js
+++ bin/main.js
@@ -5,19 +5,37 @@ const argv = require('yargs')
   .usage('usage $0 <command> [options]')
   .command('start', 'setup simulation services')
   .example('$0 start')
+  .command('save', 'archive the current API state')
+  .example('$0 save [--name yourstate]')
+  .command('load', 'load an archived API state')
+  .example('$0 load --name yourstate')
+  .command('remove', 'delete an archived API state')
+  .example('$0 remove --name yourstate')
+  .command('list', 'output the names of archived states')
+  .example('$0 list')
   .help('h')
   .alias('h', 'help')
-  .epilog('copyright StackDown 2017')
+  .epilog('Copyright StackDown 2017')
   .argv
 
-const command = argv._[0]
+const [command, name] = argv._
+
+if (command === undefined) {
+  console.log("For help, run 'simdown help' or 'simdown -h'")
+  process.exit()
+}
+
+if (name != undefined) {
+  argv.name = name
+}
 
 const simdown = new SimDown()
 
 switch(command) {
+  
   case 'start': {
     simdown.setup({}, (err, endpoints) => {
-      let str = '\nSimDown running with endpoints:\n\n'
+      let str = '\nSimulated services running at:\n\n'
 
       for (let serviceName in endpoints) {
         const endpoint = endpoints[serviceName]
@@ -25,6 +43,54 @@ switch(command) {
       }
 
       console.log(str)
+    })
+
+    break
+  }
+
+  case 'save': {
+    const saveOpts = {
+      name: argv.name
+    }
+
+    simdown.saveState(saveOpts, (err, finalName) => {
+      console.log(`Successfully saved state ${finalName}`)
+    })
+    break
+  }
+
+  case 'load': {
+    const loadOpts = {}
+
+    simdown.loadState(argv.name, loadOpts, (err) => {
+      console.log(`Successfully loaded state ${argv.name}`)
+    })
+    
+    break
+  }
+
+  case 'remove': {
+    const removeOpts = {}
+
+    simdown.removeState(argv.name, removeOpts, (err) => {
+      console.log(`Successfully removed state ${argv.name}`)
+    })
+    
+    break
+  }
+
+  case 'list': {
+    simdown.listStates({}, (err, states) => {
+      if (states.length > 0) {
+        let str = '\nArchived states:\n\n'
+
+        for (let state of states) {
+          str += state + '\n'
+        }
+        console.log(str)
+      } else {
+        console.log('\nNo archived states found\n')
+      }
     })
 
     break
